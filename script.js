c1d6a0d8726a1c801e823383d616b8b384c083c3
fabianobarroli <fabianobarroli@gmail.com>
2021-12-13 20:02:54 -0300

    continuando

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-$.ajax({
-    url: 'https://api.nasa.gov/planetary/apod?api_key=d16lo99ncLQSHbsoRLNPgLS6ShSbvTnsjfVkNafV',
-    success: function(resposta){
-        console.log(resposta.resultado[0])
-    },
-    erro:function(erro){
-        console.log(erro)
-    }
+$('button').click( function(){
+
+    let data = $('#calendario').val();
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=d16lo99ncLQSHbsoRLNPgLS6ShSbvTnsjfVkNafV & date=${data}`,
+        type:'GET',
+
+        success: function(resposta){
+            $("#tFoto").text(resposta.title);
+            $("#fotoApi").attr(resposta.url);
+            $("#dataFoto").text(resposta.date);
+            $("#credAutor").text(resposta.copyright);
+            $("#textoExplicado").text(resposta.explanation)
+        },
+     
+    })
+    return resposta
 })
+
 
 
