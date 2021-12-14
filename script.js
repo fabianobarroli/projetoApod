a57daa2f616adbfe036920e62aac1510fc8485b3
fabianobarroli <fabianobarroli@gmail.com>
2021-12-13 22:24:50 -0300

    consertando o projeto

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ $('button').click( function(){
 
     let data = $('#calendario').val();
     $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=d16lo99ncLQSHbsoRLNPgLS6ShSbvTnsjfVkNafV & date=${data}`,
+        url: `https://api.nasa.gov/planetary/apod?api_key=d16lo99ncLQSHbsoRLNPgLS6ShSbvTnsjfVkNafV&date=${data}`,
         type:'GET',
 
         success: function(resposta){
             $("#tFoto").text(resposta.title);
-            $("#fotoApi").attr(resposta.url);
+            $("#fotoApi").attr('src', resposta.url);
             $("#dataFoto").text(resposta.date);
             $("#credAutor").text(resposta.copyright);
             $("#textoExplicado").text(resposta.explanation)
         },
      
     })
-    return resposta
+   
 })
 
 
